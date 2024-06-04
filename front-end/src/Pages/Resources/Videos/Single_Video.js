c6ab283f7d8b02992fbcb9f16116ca856cf99e1b
Zayan-dev <fa21bscs0095@maju.edu.pk>
2024-06-04 15:25:14 +0500

    Video Page added

---
+++ front-end/src/Pages/Resources/Videos/Single_Video.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../Videos/Video.css';
+
+const Single_Video = ({ source, para }) => {
+    return (
+        <div className='video'>
+            <iframe
+                width="390"
+                height="250"
+                src={source}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+            ></iframe>
+            <p>{para}</p>
+        </div>
+    );
+}
+
+export default Single_Video;
